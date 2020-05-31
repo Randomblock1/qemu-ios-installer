f005aa33f454099b97a24ddacaebe4dc2c691bcc
klaus385 <klaus385@users.noreply.github.com>
2020-05-30 18:24:41 -0700

    Update qemu-raspbian.sh
    Fix issues when running this out the gate.

--- qemu-raspbian.sh
+++ qemu-raspbian.sh
@@ -1,2 +1,7 @@
+
+# Maybe this should be #!/usr/bin/env bash ¯\_(ツ)_/¯
+#!/bin/bash 
+
+# Move to qemu-pi created dir
 cd /var/mobile/qemu-pi
 qemu-system-arm -kernel ./kernel-* -append "root=/dev/sda2 rootfstype=ext4 rw" -hda ./raspbianlite.qcow -cpu arm1176 -m 256 -M versatilepb -no-reboot -serial stdio -dtb versatile-pb.dtb --accel tcg,thread=multi -net nic -net user,hostfwd=tcp::5555-:22
